329e40b623131845f458701c7f121b3df67563f8
ChenYu Xiao <apple.iiiyu@gmail.com>
2021-05-14 15:42:58 +1200

    add react router dom library

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import NotFound from './views/NotFound';
+import Login from './views/Login';
 
 function App() {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/404" component={NotFound} />
+        <Route path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
   );
 }
 
